abaa9893accd117c2f5c113cba4bc26d9c420257
Admin <dus.roman13@gmail.com>
2026-06-29 08:00:09 +0300

    Finish project modification

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,33 +1,26 @@
 package org.skypro.skyshop.product;
 
- public class Product {
-     private String name; // Название продукта
-     private int price; // Цена продукта
+public class Product {
+    private final String name; // Название продукта
+    private final int price; // Цена продукта
 
-     // Конструктор класса Product
-     public Product(String name, int price) {
-         this.name = name; // Инициализируем поле name
-         this.price = price; // Инициализируем поле price
-     }
+    // Конструктор класса Product
+    public Product(String name, int price) {
+        this.name = name; // Инициализируем поле name
+        this.price = price; // Инициализируем поле price
+    }
 
-     // Метод для получения названия продукта
-     public String getName() {
-         return this.name; // Возвращаем название продукта
-     }
+    // Метод для получения названия продукта
+    public String getName() {
+        return this.name; // Возвращаем название продукта
+    }
 
-     // Метод для установки названия продукта
-     public void setName(String name) {
-         this.name = name; // Устанавливаем новое название продукта
-     }
+    // Метод для получения цены продукта
+    public int getPrice() {
 
-     // Метод для получения цены продукта
-     public int getPrice() {
-         return this.price; // Возвращаем цену продукта
-     }
+        return this.price; // Возвращаем цену продукта
+}
+}
 
-     // Метод для установки цены продукта
-     public void setPrice(int price) {
-         this.price = price; // Устанавливаем новую цену продукта
-     }
- }
+
 
